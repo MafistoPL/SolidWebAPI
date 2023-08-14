f7707db82cf7eef5c5af759d513b8f0fbc9b66d9
MafistoPL <mateusz.poreba@outlook.com>
2023-08-14 11:34:13 +0200

    synchronous

--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var dbConnector = new DbConnector();
+var webServiceConnector = new WebServiceConnector();
+
+Console.WriteLine("Starting fetching data...");
+
+var dbResult = dbConnector.GetResult();
+var webServiceResult = webServiceConnector.GetResult();
+
+Console.WriteLine("Fetching data completed");
+
+Console.WriteLine(dbResult.Value + webServiceResult.Value);
+
+// C# 1.0 Begin/Invoke
+// C# events: += args => { ... }
+// C# async/await
+
+public class DbConnector
+{
+    public Result GetResult()
+    {
+        Console.WriteLine("Starting processing DB...");
+        Thread.Sleep(2_000);
+        Console.WriteLine("Finished processing DB...");
+
+        return new Result(1);
+    }
+}
+
+public class WebServiceConnector
+{
+    public Result GetResult()
+    {
+        Console.WriteLine("Starting processing WS...");
+        Thread.Sleep(300);
+        Console.WriteLine("Starting processing WS...");
+
+        return new Result(3);
+    }
+}
+
+public record Result(int Value);
